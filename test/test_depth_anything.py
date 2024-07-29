3231ddc7458c8ffd5bbf2583edf403fa063998ac
Optimax14 <itaykadosh10@gmail.com>
2024-07-29 17:03:36 -0500

    add input from folder and saving output

--- test/test_depth_anything.py
+++ test/test_depth_anything.py
@@ -2,32 +2,46 @@
 # Work done while being at the Intelligent Robotics and Vision Lab at the University of Texas, Dallas
 # Please check the licenses of the respective works utilized here before using this script.
 
+
+import os
 from absl import (app, logging)
 from PIL import Image as PILImg
 from robokit.utils import apply_matplotlib_colormap
 from robokit.perception import DepthAnythingPredictor
 
+directory_path = '/Path/to/INPUT/folder'
+output_directory_path = '/Path/to/OUTPUT/folder'
 
-def main(argv):
+
+def main(local_path, output_path):
     # Path to the input image
-    image_path = argv[0]
+
+    os.makedirs(output_path, exist_ok=True)
+
+    images = [os.path.join(local_path, f) for f in os.listdir(local_path)]
+
 
     try:
-        logging.info("Initialize object detectors")
-        depth_any = DepthAnythingPredictor()
+        for image_path in images:
+            
+            logging.info("Initialize object detectors")
+            depth_any = DepthAnythingPredictor()
 
-        logging.info("Open the image and convert to RGB format")
-        image_pil = PILImg.open(image_path).convert("RGB")
-        w, h =image_pil.size
+            logging.info("Open the image and convert to RGB format")
+            image_pil = PILImg.open(image_path).convert("RGB")
+            w, h =image_pil.size
 
-        logging.info("Depth Anything prediction")
-        depth_pil, raw_depth_output = depth_any.predict(image_pil)
+            logging.info("Depth Anything prediction")
+            depth_pil, raw_depth_output = depth_any.predict(image_pil)
 
-        logging.info("Convert depth values to heatmap format")
-        # colormap ref: https://github.com/yuki-koyama/pycolormap?tab=readme-ov-file
-        depth_to_colomap_pil = apply_matplotlib_colormap(depth_pil, colormap_name='inferno')
+            logging.info("Convert depth values to heatmap format")
+            # colormap ref: https://github.com/yuki-koyama/pycolormap?tab=readme-ov-file
+            depth_to_colomap_pil = apply_matplotlib_colormap(depth_pil, colormap_name='inferno')
 
-        depth_to_colomap_pil.show()
+            # output_image_path = os.path.join(output_path, os.path.basename(image_path))
+            # depth_to_colomap_pil.save(output_image_path)
+            depth_to_colomap_pil.show()
+            # print(f"Saved overlayed image to {output_image_path}")
 
     except Exception as e:
         # Handle unexpected errors
@@ -38,4 +52,5 @@ if __name__ == "__main__":
     # Run the main function with the input image path
     # app.run(main, ['imgs/color-000078.png'])
     # app.run(main, ['imgs/color-000019.png'])
-    app.run(main, ['imgs/irvl-clutter-test.png'])
+    # app.run(main, ['imgs/irvl-clutter-test.png'])
+    main(directory_path, output_directory_path)
